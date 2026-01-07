573977f86e18d424b6381f90c93ebe31394edfe1
Joana <joana.spadaccini95@gmail.com>
2026-01-07 16:19:16 -0300

    teste

---
+++ mapa.js
@@ -0,0 +1,12 @@
+const map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-46.6333, -23.5505]),
+    zoom: 10
+  })
+});
